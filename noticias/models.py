aaf120951843855abc9b7c0baf6c21c0f9159787
ze <ze.maria83@gmail.com>
2026-04-06 20:00:07 +0100

    adicionado modelo de artigo e comentario

--- noticias/models.py
+++ noticias/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+#criar classe para os artigos
+
+class Artigo(models.Model):
+    titulo = models.CharField(max_length=200)
+    corpo = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.titulo
+
+#criar classe para os comentarios
+
+class Comentario(models.Model):
+    artigo = models.ForeignKey(Artigo, on_delete=models.CASCADE, related_name='comentarios')
+    texto = models.TextField()
+    data_criacao = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Comentário em {self.artigo.titulo}"
